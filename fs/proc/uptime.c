273a8d8e4a3bba8e62008b3cb49e392acdbfed0c
Chenggang <chenggang.qin@linux.alibaba.com>
2015-11-10 09:27:55 +0800

    cgroup/namespace: Fix /proc/uptime in container
    In kernel, /proc/uptime shows in linux container is now physical machine's.
    So that commands such as uptime, top, w, show wrong uptime to users.
    
    This patch uses init task's start_time in container to calculate the uptime
    for linux container, and also corrects the idletime.
    
    Signed-off-by: Chenggang <chenggang.qin@linux.alibaba.com>

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -6,12 +6,30 @@
 #include <linux/time.h>
 #include <linux/kernel_stat.h>
 #include <linux/cputime.h>
+#include <linux/pid_namespace.h>
+
+#ifdef CONFIG_CGROUP_CPUACCT
+extern bool task_in_nonroot_cpuacct(struct task_struct *);
+extern struct kernel_cpustat *task_ca_kcpustat_ptr(struct task_struct*, int);
+#else
+bool task_in_nonroot_cpuacct(struct task_struct *tsk)
+{
+	return false;
+}
+
+struct kernel_cpustat *task_ca_kcpustat_ptr(struct task_struct*, int)
+{
+	return NULL;
+}
+#endif
 
 static int uptime_proc_show(struct seq_file *m, void *v)
 {
 	struct timespec uptime;
 	struct timespec idle;
 	u64 idletime;
+	struct task_struct *init_tsk;
+	struct kernel_cpustat *kcpustat;
 	u64 nsec;
 	u32 rem;
 	int i;
@@ -21,6 +39,27 @@ static int uptime_proc_show(struct seq_file *m, void *v)
 		idletime += (__force u64) kcpustat_cpu(i).cpustat[CPUTIME_IDLE];
 
 	get_monotonic_boottime(&uptime);
+
+	/* instance view in container */
+	if (in_noninit_pid_ns(current) &&
+		task_in_nonroot_cpuacct(current)) {
+		for_each_possible_cpu(i) {
+			kcpustat = task_ca_kcpustat_ptr(current, i);
+			/*
+		 	* Cause that CPUs set to this namespace may be changed,
+		 	* the real idle for this namespace is complicated.
+		 	*
+		 	* Just count the global idletime after this namespace
+		 	* starts. When namespace is idle, but in global
+		 	* there still have tasks running, the idle won't be
+		 	* calculated in.
+		 	*/
+			idletime -= kcpustat->cpustat[CPUTIME_IDLE_BASE];
+		}
+		init_tsk = task_active_pid_ns(current)->child_reaper;
+		uptime = timespec_sub(uptime, ns_to_timespec(init_tsk->start_time));
+	}
+
 	nsec = cputime64_to_jiffies64(idletime) * TICK_NSEC;
 	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
 	idle.tv_nsec = rem;
