78f147221218e7297448980059a9da818d18aadb
Eric Dumazet <edumazet@google.com>
2017-10-27 07:47:21 -0700

    tcp: Namespace-ify sysctl_tcp_nometrics_save
    commit ec36e416f06f6a8659281053fdc46ce484ad2211 upstream
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: George Zhang <georgezhang@linux.alibaba.com>
    Reviewed-by: Gavin Shan <shan.gavin@linux.alibaba.com>

--- net/ipv4/tcp_metrics.c
+++ net/ipv4/tcp_metrics.c
@@ -20,8 +20,6 @@
 #include <net/tcp.h>
 #include <net/genetlink.h>
 
-int sysctl_tcp_nometrics_save __read_mostly;
-
 static struct tcp_metrics_block *__tcp_get_metrics(const struct inetpeer_addr *saddr,
 						   const struct inetpeer_addr *daddr,
 						   struct net *net, unsigned int hash);
@@ -375,7 +373,7 @@ void tcp_update_metrics(struct sock *sk)
 	u32 val;
 	int m;
 
-	if (sysctl_tcp_nometrics_save || !dst)
+	if (net->ipv4.sysctl_tcp_nometrics_save || !dst)
 		return;
 
 	if (dst->flags & DST_HOST)
