a5dcad70ebf8823ed9dd7c2698314152fa11d171
shidao.ytt <shidao.ytt.kernel@linux.alibaba.com>
2017-12-18 21:27:33 +0800

    mm: workingset: tell cache transitions from workingset thrashing
    Refaults happen during transitions between workingsets as well as
    in-place thrashing. Knowing the difference between the two has a range
    of applications, including measuring the impact of memory shortage on
    the system performance, as well as the ability to smarter balance
    pressure between the filesystem cache and the swap-backed workingset.
    
    During workingset transitions, inactive cache refaults and pushes out
    established active cache. When that active cache isn't stale, however,
    and also ends up refaulting, that's bonafide thrashing.
    
    Introduce a new page flag that tells on eviction whether the page has
    been active or not in its lifetime. This bit is then stored in the
    shadow entry, to classify refaults as transitioning or thrashing.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: shidao.ytt <shidao.ytt.kernel@linux.alibaba.com>
    Reviewed-by: Oliver Yang <oliver.yang@linux.alibaba.com>
    Reviewed-by: Yang Shi <yang.shi@linux.alibaba.com>

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -79,6 +79,7 @@ enum pageflags {
 	PG_dirty,
 	PG_lru,
 	PG_active,
+	PG_workingset,
 	PG_slab,
 	PG_owner_priv_1,	/* Owner use. If pagecache, fs may use*/
 	PG_arch_1,
@@ -262,6 +263,8 @@ PAGEFLAG(Dirty, dirty, PF_HEAD) TESTSCFLAG(Dirty, dirty, PF_HEAD)
 PAGEFLAG(LRU, lru, PF_HEAD) __CLEARPAGEFLAG(LRU, lru, PF_HEAD)
 PAGEFLAG(Active, active, PF_HEAD) __CLEARPAGEFLAG(Active, active, PF_HEAD)
 	TESTCLEARFLAG(Active, active, PF_HEAD)
+PAGEFLAG(Workingset, workingset, PF_HEAD)
+	TESTCLEARFLAG(Workingset, workingset, PF_HEAD)
 __PAGEFLAG(Slab, slab, PF_NO_TAIL)
 __PAGEFLAG(SlobFree, slob_free, PF_NO_TAIL)
 PAGEFLAG(Checked, checked, PF_NO_COMPOUND)	   /* Used by some filesystems */
