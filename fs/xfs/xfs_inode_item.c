c4f3f380ef9ce882e071b81f7f08eba739fc901c
Ning Zhang <ningzhang@linux.alibaba.com>
2018-02-05 17:34:06 +0800

    xfs: use uuid_copy() helper to abstract uuid_t
    upstream dfd7487e991e
    
    uuid_t definition is about to change.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Ning Zhang <ningzhang@linux.alibaba.com>
    Reviewed-by: Gavin Shan <shan.gavin@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- fs/xfs/xfs_inode_item.c
+++ fs/xfs/xfs_inode_item.c
@@ -893,8 +893,7 @@ xfs_inode_item_format_convert(
 	in_f->ilf_dsize = in_f32->ilf_dsize;
 	in_f->ilf_ino = in_f32->ilf_ino;
 	/* copy biggest field of ilf_u */
-	memcpy(in_f->ilf_u.ilfu_uuid.__u_bits,
-	       in_f32->ilf_u.ilfu_uuid.__u_bits, sizeof(uuid_t));
+	uuid_copy(&in_f->ilf_u.ilfu_uuid, &in_f32->ilf_u.ilfu_uuid);
 	in_f->ilf_blkno = in_f32->ilf_blkno;
 	in_f->ilf_len = in_f32->ilf_len;
 	in_f->ilf_boffset = in_f32->ilf_boffset;
